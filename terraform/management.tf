746b7cee6fd6b366e120570eb8c5d06fee6f3e64
rafstef <raffaele.stefanile@gmail.com>
2025-04-15 17:08:48 +0200

    test

--- terraform/management.tf
+++ terraform/management.tf
@@ -1,35 +1,35 @@
 # Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 # #
-# module "management" {
-#   source = "./modules/aft-account-request"
+module "management" {
+  source = "./modules/aft-account-request"
 
-#   control_tower_parameters = {
-#     AccountEmail = "aws-capgemini@stefanile.com"
-#     AccountName  = "aws-capgemini"
-#     # Syntax for top-level OU
-#     ManagedOrganizationalUnit = "Root"
-#     # Syntax for nested OU
-#     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-#     SSOUserEmail     = "do-not-use@example.com"
-#     SSOUserFirstName = "do-not-use"
-#     SSOUserLastName  = "do-not-use"
-#   }
+  control_tower_parameters = {
+    AccountEmail = "aws-capgemini@stefanile.com"
+    AccountName  = "aws-capgemini"
+    # Syntax for top-level OU
+    ManagedOrganizationalUnit = "Root"
+    # Syntax for nested OU
+    # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
+    SSOUserEmail     = "do-not-use@example.com"
+    SSOUserFirstName = "do-not-use"
+    SSOUserLastName  = "do-not-use"
+  }
 
-#   account_tags = {
-#     "ABC:Owner"       = "owner@example.com"
-#     "ABC:Environment" = "Prod"
-#     "ABC:Project"     = "LandingZone"
-#   }
+  account_tags = {
+    "ABC:Owner"       = "owner@example.com"
+    "ABC:Environment" = "Prod"
+    "ABC:Project"     = "LandingZone"
+  }
 
-#   change_management_parameters = {
-#     change_requested_by = "John Doe"
-#     change_reason       = "importing log archive account"
-#   }
+  change_management_parameters = {
+    change_requested_by = "John Doe"
+    change_reason       = "importing log archive account"
+  }
 
-#   custom_fields = {
-#   }
+  custom_fields = {
+  }
 
-#   account_customizations_name = "management"
-# }
+  account_customizations_name = "management"
+}
 
